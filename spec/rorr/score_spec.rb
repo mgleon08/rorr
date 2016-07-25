221c45e25e5d96e3711632891b12e6fc01ac4c7e
Leon <mgleon08@gmail.com>
2016-07-25 22:23:15 +0800

    Add score rspec

--- spec/rorr/score_spec.rb
+++ spec/rorr/score_spec.rb
@@ -1,4 +1,34 @@
 require 'spec_helper'
 
 describe Rorr::Score do
+  subject(:score) { described_class }
+
+  it '#init' do
+    expect(score.correct).to eq(0)
+    expect(score.wrong).to eq(0)
+    expect(score.skip).to eq(0)
+  end
+
+  it '#add_correct' do
+    score.add_correct
+    expect(score.correct).to eq(1)
+  end
+
+  it '#add_wrong' do
+    score.add_wrong
+    expect(score.wrong).to eq(1)
+  end
+
+  it '#add_skip' do
+    score.add_skip
+    expect(score.skip).to eq(1)
+  end
+
+  it '#total' do
+    expect(score.total).to eq(3)
+  end
+
+  it '#correct_rate' do
+    expect(score.correct_rate).to eq((1.0/3.0)*100)
+  end
 end
