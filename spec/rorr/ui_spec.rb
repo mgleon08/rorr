df941dc17d5681c7ea6efd95603496c4147664ce
Leon <mgleon08@gmail.com>
2016-07-25 22:05:50 +0800

    Add UI rspec

--- spec/rorr/ui_spec.rb
+++ spec/rorr/ui_spec.rb
@@ -1,4 +1,36 @@
 require 'spec_helper'
 
 describe Rorr::UI do
+  subject(:ui) { described_class }
+
+  before(:each) do
+    @in     = StringIO.new
+    @out    = StringIO.new
+    @config = Rorr::Config
+    @config.stdin  = @in
+    @config.stdout = @out
+  end
+
+  it "should add puts to out stream" do
+    ui.puts "foo"
+    expect(@out.string).to eq("foo\n")
+  end
+
+  it "should fetch gets from in stream" do
+    @in.puts "bar"
+    @in.rewind
+    expect(ui.gets).to eq("bar")
+  end
+
+  it "should return empty string if no input" do
+    @config.stdin = nil
+    expect(ui.gets).to eq("")
+  end
+
+  it "should delay after puts when specified" do
+    @config.delay = 0.8
+    allow(ui).to receive(:sleep)
+    ui.puts_with_delay("foo")
+    expect(ui).to have_received(:sleep)
+  end
 end
