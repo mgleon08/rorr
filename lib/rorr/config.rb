68a6b066256c4125ab7d2a317f0b4346e0873374
Leon Ji <mgleon08@gmail.com>
2016-07-30 00:14:21 +0800

    Add defficult command line option

--- lib/rorr/config.rb
+++ lib/rorr/config.rb
@@ -2,7 +2,7 @@ module Rorr
   class Config
     @stdin, @stdout, @delay, @level = STDIN, STDOUT, 0.6, 'normal'
     class << self
-      attr_accessor :stdin, :stdout, :delay, :topic
+      attr_accessor :stdin, :stdout, :delay, :topic, :level
     end
   end
 end
--- lib/rorr/init.rb
+++ lib/rorr/init.rb
@@ -15,6 +15,7 @@ module Rorr
       options = OptionParser.new
       options.banner = 'Usage: RorR [options]'
       options.on('-s', '--second SECONDS', 'Delay each turn by seconds (default: 0.6)') { |seconds| Config.delay = seconds.to_f }
+      options.on('-d', '--difficult', 'Difficult level on RorR (default: NORMAL)') { |level| Config.level = 'difficult' }
       options.on('-h', '--help', 'Show this message') { puts(options); exit }
       options.parse!(@arguments)
     end
