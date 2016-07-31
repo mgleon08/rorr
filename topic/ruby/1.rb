895befda837dedd9aa9411dfe194121462641ee8
Leon Ji <mgleon08@gmail.com>
2016-07-31 15:33:19 +0800

    Rename topic file name and format

--- topic/ruby/1.rb
+++ topic/ruby/1.rb
@@ -1,7 +1,5 @@
-
 Yukihiro Matsumoto
 
 #=====
-
 Ruby is a dynamic, reflective, object-oriented, general-purpose programming language.
 It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan.
