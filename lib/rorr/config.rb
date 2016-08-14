2ce0af97f37be73f8c706bb4242128463e2a005b
Leon Ji <mgleon08@gmail.com>
2016-08-14 20:12:53 +0800

    Alter STDIO STDOUT to

--- lib/rorr/config.rb
+++ lib/rorr/config.rb
@@ -1,6 +1,6 @@
 module Rorr
   class Config
-    @stdin, @stdout, @delay, @level, @number, @path_prefix = STDIN, STDOUT, 0.6, 'normal', -1, "."
+    @stdin, @stdout, @delay, @level, @number, @path_prefix = $stdin, $stdout, 0.6, 'normal', -1, "."
     class << self
       attr_accessor :stdin, :stdout, :delay, :topic, :level, :number, :path_prefix
     end
