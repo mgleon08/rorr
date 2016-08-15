beb6656726451e2ef2592ec805c0fabae597c306
Leon Ji <mgleon08@gmail.com>
2016-08-15 19:59:06 +0800

    Alert default to 10 questions

--- lib/rorr/config.rb
+++ lib/rorr/config.rb
@@ -1,6 +1,6 @@
 module Rorr
   class Config
-    @stdin, @stdout, @delay, @level, @number, @path_prefix, @solution = $stdin, $stdout, 0.6, 'normal', -1, ".", false
+    @stdin, @stdout, @delay, @level, @number, @path_prefix, @solution = $stdin, $stdout, 0.6, 'normal', 9, ".", false
     class << self
       attr_accessor :stdin, :stdout, :delay, :topic, :level, :number, :path_prefix, :solution
     end
