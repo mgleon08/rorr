27708d24aa3e2a5989392461757577f29e2e28db
Leon Ji <mgleon08@gmail.com>
2016-08-26 19:52:20 +0800

    Fix config all questions arg is 0

--- lib/rorr/init.rb
+++ lib/rorr/init.rb
@@ -16,7 +16,7 @@ module Rorr
       options.banner = 'Usage: rorr [options]'
       options.on('-t', '--time SECONDS', 'Delay each turn by seconds (default: 0.6)') { |seconds| Config.delay = seconds.to_f }
       options.on('-d', '--difficult', 'Difficult level on RorR (default: NORMAL)') { |level| Config.level = 'difficult' }
-      options.on('-n', '--number NUMBER', 'Number of questions (default: 10, all = -1)') { |number| Config.number = ((number.to_i) -1) }
+      options.on('-n', '--number NUMBER', 'Number of questions (default: 10, all = 0)') { |number| Config.number = ((number.to_i) -1) }
       options.on('-s', '--solution', 'Show the solution (default: false)') { |number| Config.solution = true }
       options.on('-h', '--help', 'Show this message') { puts(options); exit }
       options.parse!(@arguments)
